3d0fe73c847c628db38519388e68390eb5a8bc48
Nathan <nathan@2le.net>
2022-05-04 15:32:43 +0200

    feat: remove stof doctrine extensions

--- src/Entity/EntityFile.php
+++ src/Entity/EntityFile.php
@@ -4,14 +4,10 @@ namespace Lle\EntityFileBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Lle\EntityFileBundle\Entity\Trait\EntityFileTrait;
-use Gedmo\Blameable\Traits\BlameableEntity;
-use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 class EntityFile implements EntityFileInterface
 {
     use EntityFileTrait;
-    use BlameableEntity;
-    use TimestampableEntity;
 
     #[ORM\Id]
     #[ORM\GeneratedValue]
